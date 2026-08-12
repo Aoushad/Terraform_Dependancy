6dc1fba642ef7d8822a52c24e926872eeddefa29
Aoushad Khan (EXT-Nokia) <aoushad.khan.ext@nokia.com>
2026-08-12 22:36:31 +0530

    dependancy

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "RG-Aoushad"
+  location = "East US"
+}
+resource "azurerm_storage_account" "stg" {
+# depends_on              = [azurerm_resource_group.rg]
+  name                     = "stgaccountaoushad"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
