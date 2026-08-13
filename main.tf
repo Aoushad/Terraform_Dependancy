fcb2a367d5e30bd50cf8d2e0c1dc70d6114587ea
Aoushad Khan (EXT-Nokia) <aoushad.khan.ext@nokia.com>
2026-08-13 07:47:36 +0530

    Add Terraform GitHub Actions workflow

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 }
 resource "azurerm_storage_account" "stg" {
-# depends_on              = [azurerm_resource_group.rg]
+  # depends_on              = [azurerm_resource_group.rg]
   name                     = "stgaccountaoushad"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
